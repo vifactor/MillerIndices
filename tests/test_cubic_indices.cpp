ec1ac9ae9ab3089c00f3895e896977409575b91a
Viktor Kopp <vifactor@gmail.com>
2014-06-16 18:43:26 +0200

    *.hpp files are also installed now.

--- tests/test_cubic_indices.cpp
+++ tests/test_cubic_indices.cpp
@@ -37,7 +37,6 @@ TEST_F(MillerIndexCubTest, dindex_create)
 
 TEST_F(MillerIndexCubTest, rindex_create)
 {
-    //MillerReciprocalCubIndices rindex;
     MillerReciprocalCubIndices dindex(1, 1, 1);
     Vector3d vec = transformator->toVector3d(dindex);
     double val = 2 * M_PI * 0.5;
@@ -47,7 +46,6 @@ TEST_F(MillerIndexCubTest, rindex_create)
 
 TEST_F(MillerIndexCubTest, rindex_dindex_multiply)
 {
-    //MillerReciprocalCubIndices rindex;
     MillerDirectCubIndices dindex(1, 1, 1);
     MillerReciprocalCubIndices rindex(1, 1, 1);
     Vector3d dvec = transformator->toVector3d(dindex);
@@ -58,6 +56,29 @@ TEST_F(MillerIndexCubTest, rindex_dindex_multiply)
                 + dindex.Z * rindex.L);
     
     EXPECT_EQ(inner_prod(rvec, dvec), val);
+}
+
+TEST_F(MillerIndexCubTest, burgers_estimate)
+{
+    MillerDirectCubIndices burgers(1, 1, 0);
+    MillerDirectCubIndices line(1, -1, 0);
+    
+    Vector3d burg_vec = transformator->toVector3d(burgers);
+    Vector3d line_vec = transformator->toVector3d(line);
+    Vector3d norm_vec = Vector3d(0, 0, 1);
+    Vector3d misf_vec;
+    
+    line_vec = normalize(line_vec);
+    misf_vec = cross(norm_vec, line_vec);
+    
+    /*misfit, screw and bz-edge components of the dislocation*/
+    double bx = inner_prod(burg_vec, misf_vec);
+    double by = inner_prod(burg_vec, line_vec);
+    double bz = inner_prod(burg_vec, norm_vec);
+    
+    EXPECT_NEAR(bx, 2 * sqrt(2), 1e-4);
+    EXPECT_EQ(by, 0);
+    EXPECT_EQ(bz, 0);
 }
 
 }//namespace
